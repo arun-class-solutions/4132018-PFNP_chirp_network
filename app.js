50e592bc477401ac4566656a5521825120c3d47a
Arun Sood <arsood@gmail.com>
2018-04-13 14:52:23 -0700

    Every route except delete

--- app.js
+++ app.js
@@ -26,13 +26,56 @@ app.get("/", function(req, res) {
 });
 
 //Get all chirps
+app.get("/chirps", (req, res) => {
+  // Step 1: Retrieve all chirps from the DB
+  models.Chirp.findAll().then((chirps) => {
+    // Step 2: Generate HTML with the chirps inside
+    // Step 3: Send the generated HTML to the browser
+    res.render("index", { chirps });
+  });
+});
 
 //Create new chirp
+app.post("/chirps", (req, res) => {
+  // Step 1: Retrieve new chirp text from the HTML form
+  var newChirp = req.body;
+
+  // Step 2: Save new chirp to the DB
+  models.Chirp.create(newChirp).then(() => {
+    // Step 3: Redirect user back to show all chirps page
+    res.redirect("/chirps");
+  });
+});
 
 //Get specific chirp
+app.get("/chirps/:id/edit", (req, res) => {
+  // Step 1: Retrieve specific chirp from the DB via its ID coming from the URL
+  var chirpId = req.params.id;
+
+  models.Chirp.findById(chirpId).then((chirp) => {
+    // Step 2: Generate HTML with single chirp inside
+    // Step 3: Send the generated HTML back to the browser
+    res.render("edit", { chirp });
+  });
+});
 
 //Edit a chirp
+app.put("/chirps/:id", (req, res) => {
+  // Step 1: Retrieve updated chirp text from the form
+  var updatedChirp = req.body;
+  var chirpId = req.params.id;
+
+  // Step 2: Retrieve specific chirp from the DB via its ID in the URL
+  models.Chirp.findById(chirpId).then((chirp) => {
+    // Step 3: Save the updates to the DB
+    chirp.updateAttributes(updatedChirp).then(() => {
+      // Step 4: Redirect user back to show all chirps page
+      res.redirect("/chirps");
+    });
+  });
+});
 
 //Delete a chirp
+// Hint: .destroy() is the method to delete a record
 
 app.listen(process.env.PORT || 3000);
